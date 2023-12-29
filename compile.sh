d6039889338fe1b7fbc866a3ece1c34851183438
LisekLucian <101988460+LisekLucian@users.noreply.github.com>
2023-12-29 13:59:28 +0100

    Create compile.sh

---
+++ compile.sh
@@ -0,0 +1,22 @@
+clear
+# Start making stuff
+echo "Compiling file(s)..."
+# krnxxx.o are from the kernel folder
+# drivxxx.o are for drivers.
+nasm -f elf32 src/kernel/krnentry.asm -o krnent.o
+gcc -m32 -c src/kernel/kernel.c -o krnc.o
+gcc -m32 -c src/drivers/ports.c -o drivport.o
+gcc -m32 -c src/drivers/idt.c -o drividt.o
+gcc -m32 -c src/drivers/utils.c -o driv_utils.o
+gcc -m32 -c src/drivers/vga.c -o driv_vga.o
+ld -m elf_i386 -T src/linker.ld --allow-multiple-definition krnent.o krnc.o drivport.o drividt.o driv_utils.o driv_vga.o -o NoodlebrainOS.bin -nostdlib
+# Check the binary file is x86 multiboot file or not
+echo "Checking if BINARY is GRUB-Ready..."
+grub-file --is-x86-multiboot ChoacuryOS.bin
+# Building the ISO file
+echo "Making ISO file"
+mkdir -p isodir/boot/grub
+cp ChoacuryOS.bin isodir/boot/NoodlebrainOS.bin
+cp src/boot/grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o NoodlebrainOS.iso isodir
+
